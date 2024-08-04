e95d148d5ec1880030d3a62cb8b6caa88a898b0e
Rathod Abhishek <130862838+Abhi8486@users.noreply.github.com>
2024-08-04 13:00:59 +0530

    pig game completed

--- pig.py
+++ pig.py
@@ -6,11 +6,16 @@ def roll():
 def score_reach(players):
     max_point =20
     val =list(players.values())
-    # print(val)
     for i in val:
-        # print(i)
         if(i >= max_point):
             return True
+        
+def display(players):
+    print(f"--------------------------------------")
+    for i in players:
+        print(f"Total score of {i} is {players[i]}")
+    print(f"--------------------------------------")
+
 # here we get no of players playing game
 no_of_players = int(input('Enter number of players (2-4)'))
 if not(no_of_players <2 or no_of_players >4):
@@ -21,7 +26,6 @@ players ={}
 for i in range(no_of_players):
     name = input("enter player name:")
     players[name]=0
-print(players)
 max_point =20
 # we will make a loop which will run until one player reach max_point
 flag=0
@@ -30,44 +34,28 @@ while True:
         break
     else:
         for i in (players):
-            print(f"player {i} score : {players[i]}")
             if flag == 1:
                 break
+            display(players)
             current_score = 0
             while True:
                 if score_reach(players) == True:
                     flag = 1
                     print("game over someone reached 20 points!")
                     break
-                else:
-                    print(f"{i}'s turn")
-                    roll_hold = input("Enter yes to roll dice or Enter no to hold score:")
-                    if roll_hold.lower() == 'no':
-                        temp = players[i]
-                        temp += current_score
-                        players.update({i:temp})
-                        print(f" total scoreof player {i} : {players[i]}" )
+                print(f"{i}'s turn")
+                print(f"Current score : {current_score}" )
+                roll_hold = input("Enter yes to roll dice or Enter no to hold score:")
+                if roll_hold.lower() == 'no':
+                    temp = players[i]
+                    temp += current_score
+                    players.update({i:temp})
+                    print(f" total scoreof player {i} : {players[i]}" )
+                    break
+                elif roll_hold.lower() == 'yes':
+                    roll_dice = roll()
+                    print("you rolled a", roll_dice)
+                    if roll_dice ==1:
                         break
-                    elif roll_hold.lower() == 'yes':
-                        roll_dice = roll()
-                        print("you rolled a", roll_dice)
-                        if roll_dice ==1:
-
-                            break
-                        else:
-                            current_score += roll_dice
-
-
-
-
-
-# thisdict = {
-#   "brand": 11,
-#   "model": 15,
-#   "year": 21
-# }
-
-# if score_reach(thisdict) ==True:
-#     print("game over someone reached 20 points!")
-# else:
-#     print("game not over")
+                    else:
+                        current_score += roll_dice
